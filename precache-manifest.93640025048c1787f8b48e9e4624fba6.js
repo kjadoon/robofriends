c7418484f90585104dba1423321845fe9baa11ea
Kamran <kamranjjadoon@gmail.com>
2020-09-17 21:20:05 -0700

    Updates

---
+++ precache-manifest.93640025048c1787f8b48e9e4624fba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ce8048029505e81bddc98ffcfbedb8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5acddf61cdc1935251c2",
+    "url": "/robofriends/static/css/main.4eece9c0.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5acddf61cdc1935251c2",
+    "url": "/robofriends/static/js/main.98f16395.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
